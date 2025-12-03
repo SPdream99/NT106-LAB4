cd6240f5fabf9030fb2fbd5619c73d1d111ea267
SPdream <37748496+SPdream99@users.noreply.github.com>
2025-12-04 00:25:51 +0700

    [FEAT] Hoàn thành giao diện và vài phương thức căn bản cho bài 6

---
+++ Lab4/Lab04-Bai06/Info.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Lab04_Bai06
+{
+    partial class Info
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Ok = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Ok);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // Ok
+            // 
+            this.Ok.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Ok.Location = new System.Drawing.Point(0, 376);
+            this.Ok.Name = "Ok";
+            this.Ok.Size = new System.Drawing.Size(800, 74);
+            this.Ok.TabIndex = 1;
+            this.Ok.Text = "OK";
+            this.Ok.UseVisualStyleBackColor = true;
+            this.Ok.Click += new System.EventHandler(this.Ok_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("Merriweather", 12F);
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(20, 3, 3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(800, 377);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Info
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Info";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Ok;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Lab4/Lab04-Bai06/Info.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace Lab04_Bai06
+{
+    public partial class Info : Form
+    {
+        HttpClient client;
+        public Info(string tokenType, string accessToken)
+        {
+            InitializeComponent();
+            client = new HttpClient();
+            this.Load += (s,e) => infoGet(tokenType, accessToken);
+        }
+
+        // Xem như đây là một cli app, khi mà nhận được token thì bật form kết quả lên
+
+        async void infoGet(string tokenType, string accessToken)
+        {
+            client.DefaultRequestHeaders.Authorization = new
+            System.Net.Http.Headers.AuthenticationHeaderValue(tokenType, accessToken);
+
+            var getUserUrl = "https://nt106.uitiot.vn/api/v1/user/me";
+            var getUserResponse = await client.GetAsync(getUserUrl);
+            var getUserResponseString = await
+            getUserResponse.Content.ReadAsStringAsync();
+            JObject data = JObject.Parse(getUserResponseString);
+
+            foreach (var property in data)
+            {
+                textBox1.Text += $"{property.Key} : {property.Value}";
+            }
+        }
+
+        private void Ok_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
