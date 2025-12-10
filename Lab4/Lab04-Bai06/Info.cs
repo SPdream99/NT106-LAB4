43f21125f42e318a56bb0ed7d5de5212b79646a1
SPdream <37748496+SPdream99@users.noreply.github.com>
2025-12-10 15:52:53 +0700

    [UPDATE] Cập nhật bài 6

--- Lab4/Lab04-Bai06/Info.cs
+++ Lab4/Lab04-Bai06/Info.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net.Http;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
-using Newtonsoft.Json.Linq;
 
 namespace Lab04_Bai06
 {
@@ -35,11 +36,76 @@ namespace Lab04_Bai06
             var getUserResponseString = await
             getUserResponse.Content.ReadAsStringAsync();
             JObject data = JObject.Parse(getUserResponseString);
-
-            foreach (var property in data)
-            {
-                textBox1.Text += $"{property.Key} : {property.Value}";
-            }
+            //string email = "";
+            //string name = "";
+            //string phone = "";
+            //foreach (var property in data)
+            //{
+            //    string title = property.Key;
+            //    JToken content = property.Value;
+            //switch (property.Key)
+            //{
+            //    case "id":
+            //        title = "ID";
+            //        break;
+            //    case "email":
+            //        email = (string) content;
+            //        title = "";
+            //        content = "";
+            //        break;
+            //    case "email_verified":
+            //        title = "Email";
+            //        if ((string)content == "False") content = email + " (Not verified)";
+            //        else content = email + " (Verified)";
+            //        break;
+            //    case "first_name":
+            //        title = "";
+            //        name = (string)content;
+            //        break;
+            //    case "last_name":
+            //        title = "Name";
+            //        content = content + " " + name;
+            //        break;
+            //    case "avatar":
+            //        title = "";
+            //        break;
+            //    case "sex":
+            //        content = ((int)content == 1) ? "Male" : "Female";
+            //        break;
+            //    case "phone":
+            //        phone = (string)content;
+            //        title = "";
+            //        content = "";
+            //        break;
+            //    case "phone_verified":
+            //        title = "Phone";
+            //        if ((string)content == "False") content = phone + " (Not verified)";
+            //        else content = phone + " (Verified)";
+            //        break;
+            //    case "is_active":
+            //        title = "Active";
+            //        break;
+            //    case "is_superuser":
+            //        title = "SuperUser";
+            //        break;
+            //    default:
+            //        break;
+            //}
+            //if (title != "")
+            //{
+            //    textBox1.Text += $"{title.ToUpper()[0]}{title.Substring(1)} : {content}";
+            //    textBox1.Text += Environment.NewLine;
+            //}
+            if (data.ContainsKey("id"))
+                textBox1.Text = $"" +
+                    $"Username : {data["username"]}{(((string)data["is_superuser"] == "True") ? " (Super User)" : "")} ({(((string)data["is_active"] == "True") ? "Active" : "Unactive")})\r\n" +
+                    $"ID : {data["id"]}\r\nEmail : {data["email"]} ({(((string)data["email_verified"] == "False") ? "Not " : "")}Verified)\r\n" +
+                    $"Phone : {data["phone"]} ({(((string)data["phone_verified"] == "False") ? "Not " : "")}Verified)\r\n" +
+                    $"Name : {data["last_name"]} {data["first_name"]}\r\n" +
+                    $"Sex : {(((int)data["sex"] == 1) ? "Male" : "Female")}\r\n" +
+                    $"Birthday : {data["birthday"]}\r\n" +
+                    $"Language : {((string)data["language"]).ToUpper()[0]}{((string)data["language"]).Substring(1)}\r\n";
+            //}
         }
 
         private void Ok_Click(object sender, EventArgs e)
--- Lab4/Lab04-Bai06/Program.cs
+++ Lab4/Lab04-Bai06/Program.cs
@@ -12,11 +12,37 @@ namespace Lab04_Bai06
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Info("123", "123"));
+            string tokenType = "123";
+            string accessToken = "123";
+            if (args.Length > 0)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    {
+                        if (args[i] == "-TokenType")
+                        {
+                            try
+                            {
+                                tokenType = args[i + 1];
+                            }
+                            catch { }
+                        }
+                        if (args[i] == "-AccessToken")
+                        {
+                            try
+                            {
+                                accessToken = args[i + 1];
+                            }
+                            catch { }
+                        }
+                    }
+                }
+            }
+            Application.Run(new Info(tokenType, accessToken));
         }
     }
 }
